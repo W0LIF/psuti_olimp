0da1573061ac82e892e13bc93255229ce88d145d
W0LIF <oleg.zapyantsev@mail.ru>
2026-04-23 02:16:28 +0400

    фикс

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 
 import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
+import App from "./app/App";
 import "./styles/index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { CurrencyProvider } from "./app/components/CurrencyContext";
