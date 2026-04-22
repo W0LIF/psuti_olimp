43bf2725b6dd3636bfdd2808ada49aa7c7bfafd1
W0LIF <oleg.zapyantsev@mail.ru>
2026-04-23 02:00:37 +0400

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 import { AuthProvider } from "./context/AuthContext";
+import { CurrencyProvider } from "./app/components/CurrencyContext";
 import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
-    <App />
-    <Toaster />
+    <CurrencyProvider>
+      <App />
+      <Toaster />
+    </CurrencyProvider>
   </AuthProvider>
 );
 
